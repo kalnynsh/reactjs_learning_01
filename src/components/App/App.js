649b9c1c94a0bb9085444e5e21911671dd4692f4
kalnynsh <kda869@yandex.ru>
2018-09-06 09:08:49 +0300

    Remove Todo component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,54 +1,10 @@
 import React, { PureComponent } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Todo from '../Todo';
-
-let id = 0;
-
-function getTodoId() {
-  return ++id;
-}
 
 class App extends PureComponent {
 
-  state = {
-      inputValue: '',
-      todos: [
-        {
-          id: 0,
-          value: 'Go shopping',
-          done: true
-        }
-      ]
-    }
-
-  handleChange = event => {
-    const value = event.target.value;
-    this.setState({inputValue: value});
-  };
-
-  handleKeyDown = event => {
-    if (event.keyCode === 13) {
-      const {inputValue, todos} = this.state;
-      const newTodo = {value: inputValue, done: false, id: getTodoId()};
-
-      this.setState({inputValue: '', todos: [...todos, newTodo]});
-    }
-  };
-
-  handleClickCheckbox = id => {
-    this.setState(state => ({
-      todos: state.todos.map(
-        todo => (id === todo.id ? {...todo, done: !todo.done} : todo)
-      )
-    }));
-  };
-
-  handleDelete = id => {
-    this.setState(state => ({
-      todos: state.todos.filter(todo => id !== todo.id)
-    }));
-  };
+  state = {};
 
   render() {
     const {inputValue, todos} = this.state;
@@ -60,26 +16,7 @@ class App extends PureComponent {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="container">
-          <input type="text"
-            value={inputValue}
-            onChange={this.handleChange}
-            onKeyDown={this.handleKeyDown}
-            className="input"
-          />
-          <div>
-            {
-              todos.map(todo => (
-                <Todo
-                  onChange={this.handleClickCheckbox}
-                  onDelete={this.handleDelete}
-                  key={todo.id}
-                  id={todo.id}
-                  text={todo.value}
-                  done={todo.done}
-                />
-              ))
-            }
-          </div>
+
         </div>
       </div>
     );
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -11,20 +11,20 @@ it('renders without crashing', () => {
 
 const wrapper = shallow(<App />);
 
-describe('Test App', () => {
-  describe('Render App components', () => {
-    it('has div tag with css class App', () => {
-      expect(wrapper.find('div.App')).toHaveLength(1);
-    });
+// describe('Test App', () => {
+//   describe('Render App components', () => {
+//     it('has div tag with css class App', () => {
+//       expect(wrapper.find('div.App')).toHaveLength(1);
+//     });
 
-    it('has p tag with css class description', () => {
-      expect(wrapper.find('div.App p.description')).toHaveLength(1);
-    });
+//     it('has p tag with css class description', () => {
+//       expect(wrapper.find('div.App p.description')).toHaveLength(1);
+//     });
 
-    it('has p.description with text: Peace everyone', () => {
-      expect(wrapper.find('div.App p.description').text()).toEqual(
-        'Peace everyone.'
-      );
-    });
-  });
-});
+//     it('has p.description with text: Peace everyone', () => {
+//       expect(wrapper.find('div.App p.description').text()).toEqual(
+//         'Peace everyone.'
+//       );
+//     });
+//   });
+// });
