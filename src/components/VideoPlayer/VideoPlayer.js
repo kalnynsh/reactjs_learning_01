afb464d9de54b8fdbdcd44b6a660733c8722bf08
kalnynsh <kda869@yandex.ru>
2018-09-08 21:55:23 +0300

    Create VideoPlayer  PureComponent .

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import videoFile from './Video.mp4';
+import './VideoPlayer.css';
 
-class VideoPlayer extends Component {
-    state = {  }
+class VideoPlayer extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.playerRef = React.createRef();
+    }
+
+    playVideoHandler = () => {
+        this.playerRef.current.play();
+    };
+
+    stopVideoHandler = () => {
+        this.playerRef.current.pause();
+    };
+
     render() {
         return (
-            <div>
-
+            <div className="video-player">
+                <video
+                    src={videoFile}
+                    ref={this.playerRef}
+                    className="video-player__source"
+                    type="video/mp4"
+                />
+                <div>
+                    <button onClick={this.playVideoHandler}>Play</button>
+                    <button onClick={this.stopVideoHandler}>Stop</button>
+                </div>
             </div>
         );
     }
